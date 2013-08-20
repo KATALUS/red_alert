3da6943715465d02e1f95e84dcc68df5665a3eac
vyrak bunleang <vyrak.bunleang@gmail.com>
2013-08-20 11:18:45 -0600

    build notification from templates and exception

---
+++ lib/red_alert/notification.rb
@@ -0,0 +1,30 @@
+require 'erb'
+
+module RedAlert
+  class Notification
+    attr_reader :subject, :body
+
+    def initialize(subject, body)
+      @subject = subject
+      @body = body
+    end
+
+    class << self
+      def build(subject_template, body_template, exception, data = {})
+        subject = compile_subject subject_template, exception
+        body = compile_body body_template, exception, data
+        self.new subject, body
+      end
+
+      private
+
+      def compile_subject(template, exception)
+        template % exception
+      end
+
+      def compile_body(template, exception, data)
+        ERB.new(template).result binding
+      end
+    end
+  end
+end
---
+++ spec/red_alert/notification_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe RedAlert::Notification do
+  describe '::new' do
+    it 'initalizes' do
+       result = RedAlert::Notification.new 'test subject', 'test body'
+       result.subject.must_equal 'test subject'
+       result.body.must_equal 'test body'
+    end
+  end
+
+  describe '::build' do
+    let(:exception) { RuntimeError.new 'explosion happened' }
+    let(:subject_template) { 'BOOM! %s' }
+    let(:body_template) { 'error: <%= exception.to_s %>' }
+    let(:additional_data) { {} }
+    subject { RedAlert::Notification.build subject_template, body_template, exception, additional_data }
+
+    it 'has subject' do
+      subject.subject.must_equal 'BOOM! explosion happened'
+    end
+
+    it 'has body' do
+      subject.body.must_equal 'error: explosion happened'
+    end
+
+    describe 'with additional data for body' do
+      let(:additional_data) { { stuff: 'in body' } }
+      let(:body_template) { 'error: <%= exception.to_s %> with stuff: <%= data[:stuff] %>' }
+
+      it 'has body' do
+        subject.body.must_equal 'error: explosion happened with stuff: in body'
+      end
+    end
+  end
+end
